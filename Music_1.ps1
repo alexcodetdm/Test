289c74c00ca8e196c5c0caa206a61823dc356ded
Alexey Oglinda <alextdm@mail.ru>
2025-09-24 14:07:03 +0300

    Update

---
+++ Music_1.ps1
@@ -0,0 +1,36 @@
+﻿# Простой синтезатор с использованием системного динамика
+function Play-Note {
+    param(
+        [int]$Frequency,
+        [int]$Duration = 500
+    )
+    [Console]::Beep($Frequency, $Duration)
+}
+
+# Ноты и их частоты (в Гц)
+$notes = @{
+    'C'  = 261
+    'D'  = 293
+    'E'  = 329
+    'F'  = 349
+    'G'  = 392
+    'A'  = 440
+    'B'  = 493
+    'C2' = 523
+}
+
+# Проиграть простую мелодию
+Write-Host "Играем мелодию..." -ForegroundColor Green
+
+Play-Note -Frequency $notes['C'] -Duration 300
+Play-Note -Frequency $notes['D'] -Duration 300
+Play-Note -Frequency $notes['E'] -Duration 300
+Play-Note -Frequency $notes['F'] -Duration 300
+Play-Note -Frequency $notes['G'] -Duration 600
+Play-Note -Frequency $notes['G'] -Duration 600
+
+Play-Note -Frequency $notes['A'] -Duration 300
+Play-Note -Frequency $notes['A'] -Duration 300
+Play-Note -Frequency $notes['A'] -Duration 300
+Play-Note -Frequency $notes['A'] -Duration 300
+Play-Note -Frequency $notes['G'] -Duration 1000
---
+++ Music_2.ps1
@@ -0,0 +1,38 @@
+﻿# Самый простой вариант - использует системный динамик
+function Play-SimpleTone {
+    param(
+        [int]$Frequency,
+        [int]$DurationMs
+    )
+    
+    # Проверяем поддерживаемый диапазон частот (37-32767 Гц)
+    if ($Frequency -lt 37 -or $Frequency -gt 32767) {
+        Write-Warning "Частота $Frequency Hz вне диапазона (37-32767 Hz)"
+        return
+    }
+    
+    [Console]::Beep($Frequency, $DurationMs)
+}
+
+# Проиграть простую мелодию
+Write-Host "Играем 'В траве сидел кузнечик'..." -ForegroundColor Green
+
+$melody = @(
+    @{Freq=392; Dur=200},  # G
+    @{Freq=440; Dur=200},  # A
+    @{Freq=392; Dur=200},  # G
+    @{Freq=349; Dur=200},  # F
+    @{Freq=330; Dur=200},  # E
+    @{Freq=294; Dur=200},  # D
+    @{Freq=330; Dur=200},  # E
+    @{Freq=349; Dur=200},  # F
+    @{Freq=392; Dur=400},  # G
+    @{Freq=392; Dur=200},  # G
+    @{Freq=392; Dur=600}   # G (длинная)
+)
+
+foreach ($note in $melody) {
+    Play-SimpleTone -Frequency $note.Freq -DurationMs $note.Dur
+}
+
+Write-Host "Мелодия завершена!" -ForegroundColor Green
